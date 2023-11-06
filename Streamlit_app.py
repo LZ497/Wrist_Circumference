5aa80c1e830767f24d7d27bf3d251d22ec991eaa
Linpei <89560257+LZ497@users.noreply.github.com>
2023-11-06 15:52:38 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -217,7 +217,7 @@ elif choose == "Prediction":
             if column not in input_df.columns:
                 input_df[column] = 0
         
-        input_df = input_df[['Heightin','Weightlbs','Age','Gender','footlength']+[columns_to_add]]
+        input_df = input_df[['Heightin','Weightlbs','Age','Gender','footlength']+columns_to_add]
 
         prediction = model.predict(input_df)[0]
 
